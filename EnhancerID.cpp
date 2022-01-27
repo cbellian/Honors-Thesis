47c65a35a957f584d25af5df2f9c321472d0f267
cbellian <89554715+cbellian@users.noreply.github.com>
2022-01-27 11:04:06 -0700

    Added main, added read in file

--- EnhancerID.cpp
+++ EnhancerID.cpp
@@ -1,17 +1,29 @@
-#include <EnhancerID.hpp>
+#include "EnhancerID.hpp"
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 int readBedGraph(std::string fileName, Peak arrayOfPeaks[],int length){
-    ifstream bedIn;
+    std::ifstream bedIn;
     bedIn.open(fileName);
     if(bedIn.fail()){
         return -1;
     }
     std::string line;
+    int index = 0;
     while(getline(bedIn,line)){
-
+        std::stringstream linestream(line);
+        std::string chromNum;
+        std::string chromStart;
+        std::string chromEnd;
+        getline(linestream,chromNum,',');
+        getline(linestream,chromStart,',');
+        getline(linestream,chromEnd,',');
+        getline(linestream,line,'\n');
+        arrayOfPeaks[index].chromNum = stoi(chromNum);
+        arrayOfPeaks[index].chromStart = stoi(chromStart);
+        arrayOfPeaks[index].chromEnd = stoi(chromEnd);
     }
 
 }
---
+++ EnhancerIDDriver.cpp
@@ -0,0 +1,2 @@
+#include "EnhancerID.hpp"
+
