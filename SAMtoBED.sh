c7567cd9a0f1949054ce5d4fcb8bd1f69eea9efc
cbellian <89554715+cbellian@users.noreply.github.com>
2022-01-28 09:37:33 -0700

    convert SAM to BED

---
+++ SAMtoBED.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+convert2bed -i sam </Users/cameronbellian/Desktop/stg21_me3_align_out >/Users/cameronbellian/Desktop/stg21_me3.bed
+
+convert2bed -i sam </Users/cameronbellian/Desktop/stg22_me2_align_out >/Users/cameronbellian/Desktop/stg22_me2.bed
+
+convert2bed -i sam </Users/cameronbellian/Desktop/stg22_me2_2_align_out >/Users/cameronbellian/Desktop/stg22_me2_2.bed
+
+convert2bed -i sam </Users/cameronbellian/Desktop/stg22_me3_align_out >/Users/cameronbellian/Desktop/stg22_me3.bed
