7498d015f99379e73d9c8167ab1746290eac5590
cbellian <cabe5510@colorado.edu>
2022-02-24 12:33:29 -0700

    working on strip plot

--- VisualizeDistroInTad.r
+++ VisualizeDistroInTad.r
@@ -54,3 +54,52 @@ plot(TWIST_3B_EXON, col = "red", xlab = "Location, Centered On Gene", ylab = "Pr
 lines(TWIST_3B, col = "Green")
 legend("topleft",legend=c("Exon","Peak", "Gene Center"),col=c("red", "green", "black"),pch = c(10,10,10), box.lty = 0)
 abline(v=13139709, col ="black")
+
+AP_Dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/AP2_dist", header = FALSE)
+View(AP_Dist)
+ggplot(AP_Dist,aes(V1)) + geom_histogram()
+
+Twist2_distance <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/twist2_dist", header = FALSE)
+View(Twist2_distance)
+
+soxb11_Dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/sox11b_dist", header = FALSE)
+View(soxb11_Dist)
+
+dlxa_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/DLX_A_dist", header = FALSE)
+View(dlxa_dist)
+
+dlxb_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/DLX_B_dist", header = FALSE)
+View(dlxb_dist)
+
+dlxc_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/DLX_C_dist", header = FALSE)
+View(dlxc_dist)
+
+dlxd_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/DLX_D_dist", header = FALSE)
+View(dlxd_dist)
+
+soxe2_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/SOXE2_dist", header = FALSE)
+View(soxe2_dist)
+
+Martik_TFAP <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/MARTIK_TFAP_dist", header = FALSE)
+View(Martik_TFAP)
+
+MSXA_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/MSXA_dist", header = FALSE)
+
+MSXB_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/MSXB_dist", header = FALSE)
+
+ZICA_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/ZICA_dist", header = FALSE)
+
+BMP24A_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/BMP24A_dist", header = FALSE)
+
+BMP24B_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/BMP24B_dist", header = FALSE)
+
+BMP24C_dist <- read.delim("~/Desktop/HonorsThesisCodeRepo/Honors-Thesis/BMP24C_dist", header = FALSE)
+
+new_dist <- rbind(soxb11_Dist, Twist2_distance, AP_Dist,dlxa_dist,dlxb_dist,dlxc_dist,dlxd_dist,soxe2_dist,Martik_TFAP,MSXA_dist,MSXB_dist,ZICA_dist,BMP24A_dist,BMP24B_dist,BMP24C_dist)
+View(new_dist)
+ggplot(new_dist,aes(V1)) + geom_boxplot()
+
+stripchart(new_dist$V1)
+stripchart(Lamprey_AP2_A$V2,method="stack", col = "green",pch = 19)
+stripchart(Lamprey_AP2_A_EXON$V2,method = "stack",col = "red", pch = 19)
+
